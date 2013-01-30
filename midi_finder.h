c4c7b244b82e0b362087dbacdb3b7b2cd9ec8e03
Jay Random <jayrandom@yahoo.com>
2013-01-30 17:16:36 +0000

    Now instead of producing sound directly can also work in remapping-only mode and send the output MIDI stream to another MIDI device or (via IAC Driver) to another MIDI application

--- midi_finder.h
+++ midi_finder.h
@@ -4,6 +4,7 @@
 #import <Foundation/Foundation.h>
 #import <CoreMIDI/CoreMIDI.h>
 
+
 NSString *getName(MIDIObjectRef object) {
   // Returns the name of a given MIDIObjectRef as an NSString
   CFStringRef name = nil;
@@ -20,52 +21,94 @@ SInt32 getUniqueId(MIDIObjectRef object) {
   return value;
 }
 
-MIDIDeviceRef find_midi_device_by_name(char *query_device_name) {
 
-    NSString *NS_query_device_name = 
-        [[NSString alloc] initWithCString:query_device_name encoding:NSASCIIStringEncoding];
+MIDIEndpointRef find_midi_endpoint_by_locator_type_idx(const char *locator, int source_or_dest, ItemCount sdIndex) {
+    // the usual Mac memory BS
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-        // How many MIDI devices do we have?
-    ItemCount deviceCount = MIDIGetNumberOfDevices();
+    NSString *NS_locator = [[NSString alloc] initWithCString:locator encoding:NSASCIIStringEncoding];
 
-        // Iterate through all MIDI devices
-    for (ItemCount i = 0 ; i < deviceCount ; ++i) {
-
-        // Grab a reference to current device
-        MIDIDeviceRef midiDevice = MIDIGetDevice(i);
-
-        if([getName(midiDevice) isEqualToString: NS_query_device_name]) {
-            return midiDevice;
-        }
-    }
-    return nil;
-}
-
-MIDIEndpointRef find_midi_source_by_device_name_entity_idx_src_idx(char *query_device_name, ItemCount entity_idx, ItemCount src_idx) {
-
-    NSString *NS_query_device_name = 
-        [[NSString alloc] initWithCString:query_device_name encoding:NSASCIIStringEncoding];
+    NSArray  *split = [NS_locator componentsSeparatedByString:@"::"];
+    NSString *NS_device_name = [split objectAtIndex: 0];
+    NSString *NS_entity_name = [split objectAtIndex: 1];
 
         // How many MIDI devices do we have?
     ItemCount deviceCount = MIDIGetNumberOfDevices();
-
     
         // Iterate through all MIDI devices
-    for (ItemCount i = 0 ; i < deviceCount ; ++i) {
+    for (ItemCount deviceIndex = 0 ; deviceIndex < deviceCount ; ++deviceIndex) {
 
         // Grab a reference to current device
-        MIDIDeviceRef midiDevice = MIDIGetDevice(i);
+        MIDIDeviceRef midiDevice = MIDIGetDevice(deviceIndex);
 
-        if([getName(midiDevice) isEqualToString: NS_query_device_name]) {
+        if([getName(midiDevice) isEqualToString: NS_device_name]) {
 
-            MIDIEntityRef midiEntity = MIDIDeviceGetEntity(midiDevice, entity_idx);
+            // How many entities do we have?
+            ItemCount entityCount = MIDIDeviceGetNumberOfEntities(midiDevice);
 
-            MIDIEndpointRef midiSource = MIDIEntityGetSource(midiEntity, src_idx);
+            // Iterate through this device's entities
+            for (ItemCount entityIndex = 0 ; entityIndex < entityCount ; ++entityIndex) {
 
-            return midiSource;
+                // Grab a reference to current entity
+                MIDIEntityRef midiEntity = MIDIDeviceGetEntity(midiDevice, entityIndex);
+
+                if([getName(midiEntity) isEqualToString: NS_entity_name]) {
+
+                    MIDIEndpointRef midiEndpoint = source_or_dest
+                        ? MIDIEntityGetDestination(midiEntity, sdIndex)
+                        : MIDIEntityGetSource(midiEntity, sdIndex);
+
+                    return midiEndpoint;
+                }
+            }
         }
     }
     return nil;
 }
 
+
+const char *midi_device_list() {
+    // the usual Mac memory BS
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // How many MIDI devices do we have?
+    ItemCount deviceCount = MIDIGetNumberOfDevices();
+
+    NSMutableArray *device_list_array = [[NSMutableArray alloc] initWithCapacity:deviceCount];
+
+    // Iterate through all MIDI devices
+    for (ItemCount deviceIndex = 0 ; deviceIndex < deviceCount ; ++deviceIndex) {
+
+        // Grab a reference to current device
+        MIDIDeviceRef midiDevice = MIDIGetDevice(deviceIndex);
+
+        // How many entities do we have?
+        ItemCount entityCount = MIDIDeviceGetNumberOfEntities(midiDevice);
+
+        // An accumulating array to hold entities' names
+        NSMutableArray *entity_list_array = [[NSMutableArray alloc] initWithCapacity:entityCount];
+
+        // Iterate through this device's entities
+        for (ItemCount entityIndex = 0 ; entityIndex < entityCount ; ++entityIndex) {
+
+            // get the entity's name
+            NSString *entity_name = [NSString stringWithFormat:@"\"%@::%@\"", getName(midiDevice), getName(MIDIDeviceGetEntity(midiDevice, entityIndex))];
+            // and push it into the accumulator
+            [entity_list_array addObject: entity_name ];
+        }
+
+        // Is this device online? (Currently connected?)
+        SInt32 isOffline = 0;
+        MIDIObjectGetIntegerProperty(midiDevice, kMIDIPropertyOffline, &isOffline);
+
+        NSString *device_string = [NSString stringWithFormat:@"\t%@[%s]:\t%@",
+            getName(midiDevice),
+            (isOffline ? "offline" : "online"),
+            [entity_list_array componentsJoinedByString: @", "]
+        ];
+        [device_list_array addObject: device_string];
+    }
+
+    return [[device_list_array componentsJoinedByString: @"\n"] UTF8String];
+}
 
--- selfless_player.mm
+++ selfless_player.mm
@@ -7,6 +7,7 @@
 #import "create_aug.h"
 #import <stdlib.h>              /* atoi() */
 #import <unistd.h>              /* getopt() */
+#import <stdio.h>
 
 
 // some MIDI constants:
@@ -29,11 +30,18 @@ typedef struct {
 MIDIchanstrument target[2] = {{ 1, 0, 0, 1 }, {1, 0, 0, 1}};
 int different_chanstruments = 0;
 
-AudioUnit synthUnit;    // have to make it global and reachable by the callback routine
+AudioUnit synthUnit;    // have to make these global and reachable by the callback routine
 unsigned char mapping[99];
 int sensitivity_correction              = 40;
 int transpose_semitones                 = 0;
 int interbank_offset_octaves            = 0;
+int sound_output_reqd = 0;
+MIDIPortRef outputPort;
+MIDIEndpointRef outputEndPoint;
+Byte packet_buffer[1024];      // storage space for MIDI Packets (max 65536)
+MIDIPacketList *outputPacketList = (MIDIPacketList*)packet_buffer;
+const char *input_locator  = "AXIS-49 2A::AXIS-49 2A";
+const char *output_locator = "IAC Driver::IAC Bus 1";
 
 
     // creates any regular hexagonal mapping for AXIS-49 selfless mode
@@ -54,17 +62,31 @@ void create_selfless_mapping(unsigned char *map, int south_offset, int southeast
 }
 
 
-static void midiInputCallback (const MIDIPacketList *pktlist, void *procRef, void *srcRef) {
+OSStatus emit_midi_event(Byte b0, Byte b1, Byte b2) {
+    OSStatus status;
+    if(sound_output_reqd) {
+        status = MusicDeviceMIDIEvent(synthUnit, b0, b1, b2, 0);
+    } else {
+        MIDIPacket *outputPacket = MIDIPacketListInit(outputPacketList);
+        Byte bytes[3] = {b0, b1, b2};
+        outputPacket = MIDIPacketListAdd(outputPacketList, sizeof(packet_buffer), outputPacket, 0, 3, bytes);
+        status = MIDISend(outputPort, outputEndPoint, outputPacketList);
+    }
+    return status;
+}
+
+
+static void midiInputCallback (const MIDIPacketList *inputPacketList, void *procRef, void *srcRef) {
     
-    MIDIPacket *packet = (MIDIPacket *)pktlist->packet;
-    for (int j=0; j < pktlist->numPackets; j++) {
-        for (int i=0; i < packet->length; i+=3) {
-            int cmdchannel = packet->data[i];
+    MIDIPacket *inputPacket = (MIDIPacket *)inputPacketList->packet;
+    for (int j=0; j < inputPacketList->numPackets; ++j) {
+        for (int i=0; i < inputPacket->length; i+=3) {
+            int cmdchannel = inputPacket->data[i];
             int cmd         = cmdchannel & 0xf0;
             int origchannel = cmdchannel & 0x0f;
             if( (cmd == kMidiMessage_NoteOff) || (cmd == kMidiMessage_NoteOn)) {
-                int orignote =  packet->data[i+1];
-                int origvelo =  packet->data[i+2];
+                int orignote =  inputPacket->data[i+1];
+                int origvelo =  inputPacket->data[i+2];
 
                 int mapped_note = mapping[orignote]
                                 + ((orignote>49) ? 12*interbank_offset_octaves : 0)
@@ -74,21 +96,32 @@ static void midiInputCallback (const MIDIPacketList *pktlist, void *procRef, voi
 
                 int velocity = origvelo ? ((origvelo+sensitivity_correction<127) ? (origvelo+sensitivity_correction) : 127) : 0;
 
-                MusicDeviceMIDIEvent(synthUnit, cmd | (target_channel-1), mapped_note, velocity, 0);
+                emit_midi_event(cmd | (target_channel-1), mapped_note, velocity);
 
-                NSLog(@"midiInput\t\t[channel=%2d]\tcmd=%3s\tnote=%3d\tvelocity=%3d", target_channel, (cmdchannel&16)?"On":"Off", mapped_note, velocity);
+                printf("[channel=%2d]\tcmd=%3s\tnote=%3d\tvelocity=%3d\t---> %s\n",
+                        target_channel,
+                        (cmdchannel&16)?"On":"Off",
+                        mapped_note,
+                        velocity,
+                        sound_output_reqd ? "(sound)" : output_locator);
             } else {
-                NSLog(@"midiInput\t\t[channel=%2d]\tcmdcode=0x%1x", origchannel, cmd>>4);
+                printf("[channel=%2d]\tcmdcode=0x%1x (ignored)\n",
+                        origchannel, cmd>>4);
             }
         }
 
-        packet = MIDIPacketNext(packet);
+        inputPacket = MIDIPacketNext(inputPacket);
     }
 }
 
 
-void usage(const char *progname) {
-    NSLog(@"Usage:\n\t%s [options]\n\nthe following options are recognized:\n\n\t{-b | -c | -j | -w}\t\t\t\t\t\tmutually exclusive layout option\n\t-a south_offset:southeast_offset:start_note\t\t\tset an arbitrary isomorphic layout based on two offsets and start_note\n\t-d midi_input_device_name\t\t\t\t\t'AXIS-49 2A' by default\n\t-i midi_instrument[:midi_channel[:msb_bank[:lsb_bank]]]\t\tdefault instrument\n\t-y midi_instrument[:midi_channel[:msb_bank[:lsb_bank]]]\t\tright bank's instrument if you want a different one\n\t-o interbank_offset_octaves\t\t\t\t\t0 by default, lets you split the banks' ranges apart\n\t-s sensitivity_correction\t\t\t\t\t40 by default, the whole point of writing this program :)\n\t-t transpose_semitones\t\t\t\t\t\t0 by default", progname);
+void help(const char *progname) {
+
+    printf("Usage:\n\t%s [options]\n\nThe following options are recognized:\n\n\t{-b | -c | -j | -w}\t\t\t\t\t\tmutually exclusive layout option\n\t-a south_offset:southeast_offset:start_note\t\t\tset an arbitrary isomorphic layout based on two offsets and start_note\n\t-d midi_input_device_locator\t\t\t\t\t\"AXIS-49 2A::AXIS-49 2A\" by default\n\t-e midi_output_device_locator\t\t\t\t\t\"IAC Driver::IAC Bus 1\" by default\n\t-i midi_instrument[:midi_channel[:msb_bank[:lsb_bank]]]\t\tdefault instrument\n\t-y midi_instrument[:midi_channel[:msb_bank[:lsb_bank]]]\t\tright bank's instrument if you want a different one\n\t-o interbank_offset_octaves\t\t\t\t\t0 by default, lets you split the banks' ranges apart\n\t-s sensitivity_correction\t\t\t\t\t40 by default, the whole point of writing this program :)\n\t-t transpose_semitones\t\t\t\t\t\t0 by default\n\n", progname);
+
+    printf("Available device locators (you may need to include the quotes) :\n");
+
+    puts( midi_device_list() );
     exit(0);
 }
 
@@ -98,9 +131,8 @@ int main(int argc, char *argv[]) {
     OSStatus result;
     MIDIClientRef midiClient;
     MIDIPortRef inputPort;
-    MIDIObjectRef endPoint;
+    MIDIEndpointRef inputEndPoint;
     AUGraph graph = 0;
-    char *device_name = (char *)"AXIS-49 2A";
 
         // mapping parameters default to Sonome ("harmonic table") layout
     int south_offset    = -7;
@@ -108,7 +140,7 @@ int main(int argc, char *argv[]) {
     unsigned start_note = 81;
 
     int ch;
-    while ((ch = getopt(argc, argv, "a:bcjwd:i:y:o:s:t:")) != -1) {
+    while ((ch = getopt(argc, argv, "a:bcjwd:e:i:y:o:s:t:")) != -1) {
         switch (ch) {
             case 'a':
                     // arbitrary isomorphic hexagonal layout:
@@ -139,7 +171,11 @@ int main(int argc, char *argv[]) {
                 start_note      = 32;
                 break;
             case 'd':
-                device_name = optarg;
+                input_locator = optarg;
+                break;
+            case 'e':
+                output_locator = optarg;
+                sound_output_reqd = !output_locator[0];
                 break;
             case 'i':
                     // instrument and channel are base-1, msb_bank and lsb_bank are base-0
@@ -162,7 +198,7 @@ int main(int argc, char *argv[]) {
                 transpose_semitones = atoi(optarg);
                 break;
             default:
-                usage(argv[0]);
+                help(argv[0]);
             }
      }
 
@@ -171,35 +207,37 @@ int main(int argc, char *argv[]) {
         // Setting up MIDI part (with the callback)
     require_noerr( result = MIDIClientCreate(CFSTR("MIDI client"), NULL, NULL, &midiClient), home);
     require_noerr( result = MIDIInputPortCreate(midiClient, CFSTR("Input"), midiInputCallback, NULL, &inputPort), home);
-    endPoint = find_midi_source_by_device_name_entity_idx_src_idx( device_name, 0, 0 );
+    inputEndPoint = find_midi_endpoint_by_locator_type_idx( input_locator, 0, 0 );
 #if (MACOSX_VERSION >= 1060)
-    require_noerr( result = MIDIPortConnectSource(inputPort, endPoint, NULL), home);
+    require_noerr( result = MIDIPortConnectSource(inputPort, inputEndPoint, NULL), home);
 #else
-    require_noerr( result = MIDIPortConnectSource((OpaqueMIDIPort*)inputPort, (OpaqueMIDIEndpoint*)endPoint, NULL), home);
+    require_noerr( result = MIDIPortConnectSource((OpaqueMIDIPort*)inputPort, (OpaqueMIDIEndpoint*)inputEndPoint, NULL), home);
 #endif
+    if(!sound_output_reqd) {
+        require_noerr( result = MIDIOutputPortCreate(midiClient, CFSTR("Output"), &outputPort), home);
+
+        outputEndPoint = find_midi_endpoint_by_locator_type_idx( output_locator, 1, 0 );
+    }
 
         // Setting up sound generation
     require_noerr (result = CreateAUGraph (graph, synthUnit), home);
 
     for(int i=0;i<=different_chanstruments;i++) {
-        NSLog(@"Setting bank[%d]", i);
+        printf("Setting bank[%d]\n", i);
                 //set the bank
-        require_noerr (result = MusicDeviceMIDIEvent(synthUnit, 
-                                                    kMidiMessage_ControlChange | (target[i].channel-1), 
-                                                    kMidiMessage_BankMSBControl | target[i].msb_bank, 0,
-                                                    0/*sample offset*/), home);
-        require_noerr (result = MusicDeviceMIDIEvent(synthUnit, 
-                                                    kMidiMessage_ControlChange | (target[i].channel-1), 
-                                                    kMidiMessage_BankLSBControl | target[i].lsb_bank, 0,
-                                                    0/*sample offset*/), home);
+        require_noerr (result = emit_midi_event( kMidiMessage_ControlChange | (target[i].channel-1), 
+                                                    kMidiMessage_BankMSBControl | target[i].msb_bank, 0
+                                                    ), home);
+        require_noerr (result = emit_midi_event( kMidiMessage_ControlChange | (target[i].channel-1), 
+                                                    kMidiMessage_BankLSBControl | target[i].lsb_bank, 0
+                                                    ), home);
                 // set the instrument
-        require_noerr (result = MusicDeviceMIDIEvent(synthUnit,
-                                                    kMidiMessage_ProgramChange | (target[i].channel-1), 
-                                                    (target[i].instrument-1), 0,
-                                                    0/*sample offset*/), home);
+        require_noerr (result = emit_midi_event( kMidiMessage_ProgramChange | (target[i].channel-1), 
+                                                    (target[i].instrument-1), 0
+                                                    ), home);
     }
 
-    NSLog(@"Ready to play");
+    printf("Ready to play\n");
 
     CFRunLoopRun();
 
